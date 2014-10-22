1a14a22cc5eb156034a4b8eadcc03b3d5ace4f2c
leandrovianna <leandrovianna50@gmail.com>
2014-10-22 21:56:35 -0200

    Retirando erro no lista_palestras.xml e terminando o merge

--- gen/com/congresso/R.java
+++ gen/com/congresso/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f090041;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
@@ -1112,11 +1112,12 @@ containing a value of this type.
         public static final int image=0x7f090027;
         public static final int listMode=0x7f090006;
         public static final int list_item=0x7f090029;
+        public static final int lista=0x7f09003d;
         public static final int middle=0x7f090018;
         public static final int never=0x7f090012;
         public static final int none=0x7f090019;
         public static final int normal=0x7f090007;
-        public static final int preview_view=0x7f09003c;
+        public static final int preview_view=0x7f09003e;
         public static final int progress_circular=0x7f090004;
         public static final int progress_horizontal=0x7f090005;
         public static final int radio=0x7f09002f;
@@ -1135,14 +1136,15 @@ containing a value of this type.
         public static final int showHome=0x7f09000c;
         public static final int showTitle=0x7f09000d;
         public static final int split_action_bar=0x7f09001e;
-        public static final int status_view=0x7f09003e;
+        public static final int status_view=0x7f090040;
         public static final int submit_area=0x7f090039;
         public static final int tabMode=0x7f090008;
         public static final int title=0x7f09002b;
         public static final int top_action_bar=0x7f090020;
+        public static final int título=0x7f09003c;
         public static final int up=0x7f090021;
         public static final int useLogo=0x7f09000e;
-        public static final int viewfinder_view=0x7f09003d;
+        public static final int viewfinder_view=0x7f09003f;
         public static final int withText=0x7f090013;
     }
     public static final class integer {
@@ -1195,9 +1197,10 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int qrcode_reader_layout=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_lista_palestras=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int qrcode_reader_layout=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
