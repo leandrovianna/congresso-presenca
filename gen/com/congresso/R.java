51545e98cdc609991b8acfd4db855e8e4dfef046
leandrovianna <leandrovianna50@gmail.com>
2014-10-30 06:55:41 -0200

    Retirando coisas desnecessárias

--- gen/com/congresso/R.java
+++ gen/com/congresso/R.java
@@ -1095,7 +1095,6 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f090049;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
@@ -1213,9 +1212,6 @@ containing a value of this type.
         public static final int qrcode_reader_layout=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
-    public static final class menu {
-        public static final int activity_verificar_presenca=0x7f0c0000;
-    }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
@@ -1257,11 +1253,9 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000f;
-        public static final int msg_default_status=0x7f0a0011;
-        public static final int title_activity_verificar_presenca=0x7f0a0012;
+        public static final int msg_default_status=0x7f0a000f;
+        public static final int title_activity_verificar_presenca=0x7f0a0010;
         public static final int title_lista_palestras=0x7f0a000e;
     }
     public static final class style {
--- src/com/congresso/ActivityVerificarPresenca.java
+++ src/com/congresso/ActivityVerificarPresenca.java
@@ -40,25 +40,6 @@ public class ActivityVerificarPresenca extends Activity implements OnClickListen
 		}
 			
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_verificar_presenca, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 	
 	public void qr () {
 		
