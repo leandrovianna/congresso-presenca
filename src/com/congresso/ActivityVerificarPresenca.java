f63a9a3d8600aba4080201846075cc46af31b4f8
leandrovianna <leandrovianna50@gmail.com>
2014-11-02 17:03:35 -0200

    Botao de buscar inscrito funcionando

--- src/com/congresso/ActivityVerificarPresenca.java
+++ src/com/congresso/ActivityVerificarPresenca.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -19,85 +18,84 @@ import com.congresso.dao.ParticipacaoDAOImpl;
 public class ActivityVerificarPresenca extends Activity {
 
 	private AlertDialog confirmacao;
-	private AlertDialog.Builder builder;
-	
+
 	private EditText etInscricao;
 	private TextView tvNome, tvPalestra;
 	private ImageButton btValidar;
-	
+
 	private Participacao participacao;
 	private Ministracao ministracao;
 	private ParticipacaoDAOImpl dao;
 	private MinistracaoDAOImpl daoM;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_verificar_presenca);
-		
+
 		String id = getIntent().getStringExtra(ActivityListaPalestras.EXTRA_MINISTRACAO_ID);
-		
+
 		if (id != null) {
-			
+			constroiDialogoConfirmacao();
+
 			dao = new ParticipacaoDAOImpl(this);
 			daoM = new MinistracaoDAOImpl(this);
-			
-			builder = new AlertDialog.Builder(this);
-			iniciarMensagem();
-			
+			participacao = new Participacao();
+			ministracao = daoM.buscarMinistracaoPorId(Integer.parseInt(id));
+
 			etInscricao = (EditText) findViewById(R.id.et_inscricao);
 			tvNome = (TextView) findViewById(R.id.tv_nome);
 			btValidar = (ImageButton) findViewById(R.id.bt_validar);
 			tvPalestra = (TextView) findViewById(R.id.tvPalestra);
-			
-			tvPalestra.setText("Nome da Palestra");
-			
+
+			tvPalestra.setText(ministracao.getPalestra().getNome());
 			btValidar.setEnabled(false);
 		}
-			
+
 	}
-	
-	private void iniciarMensagem() {
-		
+
+	private void constroiDialogoConfirmacao() {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Confirmação");
 		builder.setMessage("O aluno está presente?");
-		
+
 		builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-			
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				
+
 				// conferir
-				
+
 				confirmarPresenca();
 			}
 		});
-		
+
 		builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-			
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				
+
 				return;	
 			}
 		});
-		
+
 		confirmacao = builder.create();		
 	}
 
 	protected void confirmarPresenca() {
-		
+
 		dao.removerParticipacao(dao.buscarParticipacaoPorId
 				(Integer.parseInt(etInscricao.getText().toString())));
-		
+
 		dao.inserirParticipacao(participacao);
-		
+
 		setContentView(R.layout.activity_verificar_presenca);
-		
+
 	}
 
 	public void qr(View v) {
-		
+
 		IntentIntegrator.initiateScan(this, R.layout.qrcode_reader_layout, 
 				R.id.viewfinder_view, R.id.preview_view, true);
 	}
@@ -105,39 +103,45 @@ public class ActivityVerificarPresenca extends Activity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
+
 		IntentResult result = 
 				IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-		
+
 		if (result != null) {
-			
+
 			String textoQr = result.getContents();
-			
+
 			etInscricao.setText(textoQr);
 		}
 	}
 
 	public void buscarInscrito (View v) {
 		
-		ministracao = daoM.buscarMinistracaoPorId(Integer.parseInt
-				(getIntent().getStringExtra(ActivityListaPalestras.EXTRA_MINISTRACAO_ID).toString()));
-		
-		participacao = dao.buscarParticipacaoPorInscricaoMinistracao(Integer.parseInt
-				(etInscricao.getText().toString()), ministracao);
-		
-		tvNome.setText(participacao.getParticipante().getNome());
-		
-		btValidar.setEnabled(true);
-	}
+		if (etInscricao.getText().length() != 0) {
+			
+			int inscricao = Integer.parseInt(etInscricao.getText().toString());
+
+			participacao = dao.buscarParticipacaoPorInscricaoMinistracao(
+					inscricao, ministracao);
 	
+			if (participacao != null) {
+				tvNome.setText(participacao.getParticipante().getNome());
+	
+				btValidar.setEnabled(true);
+			} else {
+				tvNome.setText("Inscrito não encontrado.");
+			}
+		}
+	}
+
 	public void validarPresenca (View v) {
-		
+
 		participacao.setPresenca(true);
 		confirmacao.show();
 	}
-	
+
 	public void voltar (View v) {
-		
+
 		setContentView(R.layout.activity_lista_palestras);
 	}
 }
--- src/com/congresso/dao/ParticipacaoDAOImpl.java
+++ src/com/congresso/dao/ParticipacaoDAOImpl.java
@@ -17,82 +17,88 @@ public class ParticipacaoDAOImpl implements ParticipacaoDAO {
 
 	private DatabaseHelper helper;
 	private SQLiteDatabase db;
-	
+
 	private MinistracaoDAOImpl ministDAO;
-	
+
 	public ParticipacaoDAOImpl(Context context) {
 		helper = new DatabaseHelper(context);
 		ministDAO = new MinistracaoDAOImpl(context);
 	}
-	
+
 	private SQLiteDatabase getDb() {
 		if (db == null)
 			db = helper.getWritableDatabase();
-		
+
 		return db;
 	}
 
 	@Override
 	public List<Participacao> listarParticipacao() {
-		
+
 		Cursor cursor = getDb().rawQuery("SELECT * FROM participacao, participante " +
 				"WHERE participacao.participante_inscricao = participante.inscricao", null);
-		
+
 		List<Participacao> participacoes = new ArrayList<Participacao>();
-		
+
 		while (cursor.moveToNext()) {
 			Participacao p = criarParticipacao(cursor);
 			participacoes.add(p);
 		}
-		
+
 		return participacoes;
 	}
 
 	private Participacao criarParticipacao(Cursor cursor) {
-		Participacao p = new Participacao();
-		
-		p.setId(cursor.getInt(cursor.getColumnIndex("_id")));
-		p.setMinistracao(ministDAO.buscarMinistracaoPorId(cursor.getInt(cursor.getColumnIndex("ministracao_id"))));
-		p.setParticipante(new Participante(
+		if (cursor.moveToNext()) {
+
+			Participacao p = new Participacao();
+
+			p.setId(cursor.getInt(cursor.getColumnIndex("_id")));
+			p.setMinistracao(ministDAO.buscarMinistracaoPorId(cursor.getInt(cursor.getColumnIndex("ministracao_id"))));
+			p.setParticipante(new Participante(
 					cursor.getInt(cursor.getColumnIndex("participante_inscricao")),
 					cursor.getString(cursor.getColumnIndex("nome"))
-				));
-		p.setPresenca(cursor.getInt(cursor.getColumnIndex("presenca")));
-		p.setUpdated(cursor.getInt(cursor.getColumnIndex("updated")));
-		
-		return p;
+					));
+			p.setPresenca(cursor.getInt(cursor.getColumnIndex("presenca")));
+			p.setUpdated(cursor.getInt(cursor.getColumnIndex("updated")));
+
+			return p;
+
+		}
+
+		return null;
 	}
 
 	@Override
 	public boolean inserirParticipacao(Participacao participacao) {
 		ContentValues values = new ContentValues();
-		
+
 		values.put("_id", participacao.getId());
 		values.put("ministracao_id", participacao.getMinistracao().getId());
 		values.put("participante_inscricao", participacao.getParticipante().getInscricao());
 		values.put("presenca", participacao.isPresenca());
 		values.put("updated", participacao.isUpdated());
-		
+
 		long retorno = getDb().insert("participacao", null, values);
-		
+
 		return retorno != -1;
 	}
 
 	@Override
 	public boolean removerParticipacao(Participacao participacao) {
 		int retorno = getDb().delete("participacao", "WHERE _id = "+participacao.getId(), null);
-		
+
 		return retorno != 0;
 	}
 
 	@Override
 	public Participacao buscarParticipacaoPorId(int id) {
 		Participacao p;
-		
+
 		Cursor cursor = getDb().rawQuery("SELECT * FROM participacao, participante " +
 				"WHERE participacao.participante_inscricao = participante.inscricao AND " +
 				"participacao._id = "+id, null);
-		
+
 		p = criarParticipacao(cursor);
 
 		return p;
@@ -101,12 +107,13 @@ public class ParticipacaoDAOImpl implements ParticipacaoDAO {
 	@Override
 	public Participacao buscarParticipacaoPorInscricaoMinistracao(
 			int inscricao, Ministracao ministracao) {
-		
+
 		Cursor cursor = getDb().rawQuery("SELECT * FROM participacao, participante " +
-				"WHERE participacao.ministracao_id = "+ministracao.getId()+" AND " +
-				"participacao.participante_inscricao = "+inscricao+" AND " +
-				"participante.inscricao = participacao.participante_inscricao", null);
-		
+				"WHERE participante.inscricao = participacao.participante_inscricao AND " +
+				"participacao.ministracao_id = "+ministracao.getId()+" AND " +
+				"participacao.participante_inscricao = "+inscricao
+				, null);
+
 		Participacao p = criarParticipacao(cursor);
 
 		return p;
