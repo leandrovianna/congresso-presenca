9c6920b614ed11ebe846293db305626c10c43c5c
leandrovianna <leandrovianna50@gmail.com>
2014-11-10 10:55:29 -0200

    Mudando link do json de importação para o Json do Ifg

--- src/com/congresso/ImportarDadosActivity.java
+++ src/com/congresso/ImportarDadosActivity.java
@@ -25,7 +25,7 @@ public class ImportarDadosActivity extends Activity implements HttpClientListene
 	private EditText etLink;
 
 	private GetHttpClientTask getHttpTask;
-	private final String link = "http://leandrovianna.net84.net/json/congresso_teste.json";
+	private final String link = "http://intranet.ifg.edu.br/eventos/admin/dadosjson.php";
 	
 	private Gson gson;
 	private ImportadoraDados importadora;
