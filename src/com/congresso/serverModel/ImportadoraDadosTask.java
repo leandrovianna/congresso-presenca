808b6087de4995b6b6b1b28c0fd3bbb0d2e3e90b
leandrovianna <leandrovianna50@gmail.com>
2014-11-12 17:36:24 -0200

    Arrumando bug de não criação das participações das ministrações de palestras de mais de um dia

--- src/com/congresso/serverModel/ImportadoraDadosTask.java
+++ src/com/congresso/serverModel/ImportadoraDadosTask.java
@@ -26,8 +26,8 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 
 	private ImportarDadosActivity ac;
 	private ParticipacaoDAOImpl dao;
-	
-	
+
+
 	@SuppressLint("SimpleDateFormat")
 	public ImportadoraDadosTask(ImportarDadosActivity ac) {
 		this.ac = ac;
@@ -37,14 +37,14 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 		dao = new ParticipacaoDAOImpl(ac);
 	}
 
-	
-	
+
+
 	@Override
 	protected Boolean doInBackground(String... params) {
 		return gravarDados(params[0]);
 	}
-	
-	
+
+
 	@Override
 	protected void onPostExecute(Boolean result) {
 		super.onPostExecute(result);
@@ -54,10 +54,10 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 
 	private boolean gravarDados(String json) {
 		db = helper.getWritableDatabase();
-		
+
 		json = json.replace("\\/", "-");
 		Evento evento = gson.fromJson(json, Evento.class);
-		
+
 		//1a fase: guardar as participações
 		List<Participacao> participacoesAntigas = dao.listarParticipacoesComPresenca();
 
@@ -66,8 +66,8 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 		db.execSQL("DELETE FROM ministracao;");
 		db.execSQL("DELETE FROM participacao;");
 		db.execSQL("DELETE FROM participante;");
-		
-		
+
+
 		//3a fase: incluir os dados do json
 		int ministracaoIdCount = 0; //contador para os ids das ministracoes
 		int participacaoIdCount = 0; //contador para os ids das participacoes
@@ -76,7 +76,7 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 
 			if (atividade == null)
 				break;
-			
+
 			ContentValues values = new ContentValues(); //criando o contentvalues que será usado
 			String data;
 
@@ -85,7 +85,7 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 				data = DateFormat.format("yyyy-MM-dd",
 						dateFormatJson.parse(atividade.getDTHORA_INICIO()))
 						.toString();
-				
+
 			} catch (ParseException e) {
 				Log.e("ERRO: ImportadoraDados",
 						"Erro no parser da data a partir do Json");
@@ -93,21 +93,22 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 			}
 			Log.i("OK: ImportadoraDados", "Sucesso. Data a ser passado pro banco: "
 					+ data);
-			
+
 
 			//verificar se a palestra NAO EXISTE
 			if ((db.rawQuery("SELECT * FROM palestra WHERE _id = "+atividade.getCODATIVIDADE(), 
 					null)).getCount() == 0) {
-				
+
 				//se a palestra nao existe, então vamos adiciona-la ao banco
-				
+
+				Log.d("ImportadoraDados", "Criando Palestra cod:"+atividade.getCODATIVIDADE());
 				
 				//criando registro Palestra no banco
 				values.put("_id", Integer.parseInt(atividade.getCODATIVIDADE()));
 				values.put("nome", atividade.getATIVIDADE());
 				db.insert("palestra", null, values);
 				values.clear();
-				
+
 				
 				//criando registro Ministracao no banco
 				values.put("_id", ministracaoIdCount);
@@ -116,19 +117,26 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 						Integer.parseInt(atividade.getCODATIVIDADE()));
 				db.insert("ministracao", null, values);
 				values.clear();
-				
+
 				for (ParticipanteServer participante : atividade
 						.getLISTA_PARTICIPANTES()) {
 
 					if (participante == null)
 						break;
 
-					//criando registro Participante no banco
-					values.put("inscricao",
-							Integer.parseInt(participante.getCODPARTICIPANTE()));
-					values.put("nome", participante.getNOME());
-					db.insert("participante", null, values);
-					values.clear();
+					if ((db.rawQuery("SELECT * FROM participante WHERE inscricao = "+participante.getCODPARTICIPANTE(), 
+							null)).getCount() == 0) {
+
+						Log.d("ImportadoraDados", "Criando Participante cod:"+participante.getCODPARTICIPANTE());
+						
+						//criando registro Participante no banco
+						values.put("inscricao",
+								Integer.parseInt(participante.getCODPARTICIPANTE()));
+						values.put("nome", participante.getNOME());
+						db.insert("participante", null, values);
+						values.clear();
+
+					}
 
 					//criando registro Participacao no banco
 					values.put("_id", participacaoIdCount);
@@ -145,7 +153,7 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 			} else {
 				//caso contrário, a palestra já existe no banco
 				//então vamos criar uma ministracao, com a nova data
-				
+
 				//criando registro Ministracao no banco
 				values.put("_id", ministracaoIdCount);
 				values.put("data", data);
@@ -153,21 +161,39 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 						Integer.parseInt(atividade.getCODATIVIDADE()));
 				db.insert("ministracao", null, values);
 				values.clear();
+
+				for (ParticipanteServer participante : atividade
+						.getLISTA_PARTICIPANTES()) {
+
+					if (participante == null)
+						break;
+
+					//criando registro Participacao no banco
+					values.put("_id", participacaoIdCount);
+					values.put("ministracao_id", ministracaoIdCount);
+					values.put("participante_inscricao",
+							Integer.parseInt(participante.getCODPARTICIPANTE()));
+					values.put("presenca", false);
+					values.put("updated", false);
+					db.insert("participacao", null, values);
+					values.clear();
+
+					participacaoIdCount++;
+				}
 			}
-			
+
 			ministracaoIdCount++;
 		}
-		
+
 		//4a fase: reinserir participações antigas
-		
+
 		for (Participacao participacao : participacoesAntigas) {
 			dao.updateParticipacao(participacao);
 		}
 		
+		db.close();
+
 		return true;
 	}
-	
-	
-	
 
 }
