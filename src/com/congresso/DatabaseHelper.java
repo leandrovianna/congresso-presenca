af11010bf66f72f3658ef73da910b3f0403eeaf3
leandrovianna <leandrovianna50@gmail.com>
2014-11-14 06:49:29 -0200

    Retirando inserção de dados teste

--- src/com/congresso/DatabaseHelper.java
+++ src/com/congresso/DatabaseHelper.java
@@ -32,164 +32,164 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		
 		db.execSQL("CREATE TABLE participante (inscricao INTEGER PRIMARY KEY, nome TEXT)");
 		
-		inserirDadosTeste(db); //inserindo dados para teste
+//		inserirDadosTeste(db); //inserindo dados para teste
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		inserirDadosTeste(db); //inserindo dados para teste
+//		inserirDadosTeste(db); //inserindo dados para teste
 	}
 	
-	private void inserirDadosTeste(SQLiteDatabase db) {
-		
-		db.execSQL("DELETE FROM palestra;");
-		db.execSQL("DELETE FROM ministracao;");
-		db.execSQL("DELETE FROM participacao;");
-		db.execSQL("DELETE FROM participante;");
-		
-		ContentValues values = new ContentValues();
-		ContentValues ministracaoValues = new ContentValues();
-		ContentValues participacoesValues = new ContentValues();
-		
-		//INSERINDO PALESTRAS E SUAS MINITRAÇÕES
-		
-		values.put("_id", 0);
-		values.put("nome", "Desenvolvimento de Jogos");
-		db.insert("palestra", null, values);
-		
-		ministracaoValues.put("_id", 0);
-		ministracaoValues.put("data", "2014-11-03");
-		ministracaoValues.put("palestra_id", 0);
-		db.insert("ministracao", null, ministracaoValues);
-		
-		ministracaoValues.put("_id", 1);
-		ministracaoValues.put("data", "2014-11-04");
-		ministracaoValues.put("palestra_id", 0);
-		db.insert("ministracao", null, ministracaoValues);
-		
-		values.put("_id", 1);
-		values.put("nome", "Minicurso Arduino");
-		db.insert("palestra", null, values);
-		
-		ministracaoValues.put("_id", 2);
-		ministracaoValues.put("data", "2014-11-03");
-		ministracaoValues.put("palestra_id", 1);
-		db.insert("ministracao", null, ministracaoValues);
-		
-		ministracaoValues.put("_id", 3);
-		ministracaoValues.put("data", "2014-11-04");
-		ministracaoValues.put("palestra_id", 1);
-		db.insert("ministracao", null, ministracaoValues);
-		
-		values.put("_id", 2);
-		values.put("nome", "Introdução Ruby");
-		db.insert("palestra", null, values);
-		
-		ministracaoValues.put("_id", 4);
-		ministracaoValues.put("data", "2014-11-03");
-		ministracaoValues.put("palestra_id", 2);
-		db.insert("ministracao", null, ministracaoValues);
-
-		values.clear();
-		
-		//INSERINDO OS PARTICIPANTES E SUAS PARTICIPAÇÕES
-		
-		values.put("inscricao", 1001);
-		values.put("nome", "João da Silva");
-		db.insert("participante", null, values);
-		
-		participacoesValues.put("_id", 0);
-		participacoesValues.put("ministracao_id", 0);
-		participacoesValues.put("participante_inscricao", 1001);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 1);
-		participacoesValues.put("ministracao_id", 1);
-		participacoesValues.put("participante_inscricao", 1001);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 2);
-		participacoesValues.put("ministracao_id", 2);
-		participacoesValues.put("participante_inscricao", 1001);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 3);
-		participacoesValues.put("ministracao_id", 3);
-		participacoesValues.put("participante_inscricao", 1001);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 4);
-		participacoesValues.put("ministracao_id", 4);
-		participacoesValues.put("participante_inscricao", 1001);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		values.put("inscricao", 1002);
-		values.put("nome", "Maria de Sousa Carvalho");
-		db.insert("participante", null, values); //minist ids 2 e 3
-		
-		participacoesValues.put("_id", 5);
-		participacoesValues.put("ministracao_id", 2);
-		participacoesValues.put("participante_inscricao", 1002);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 6);
-		participacoesValues.put("ministracao_id", 3);
-		participacoesValues.put("participante_inscricao", 1002);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-	
-		participacoesValues.put("_id", 7);
-		participacoesValues.put("ministracao_id", 4);
-		participacoesValues.put("participante_inscricao", 1002);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-	
-		values.put("inscricao", 1003);
-		values.put("nome", "Otavio Mesquita");
-		db.insert("participante", null, values);
-		//minist. ids 0, 1, 3 (jogos e ruby)
-		
-		participacoesValues.put("_id", 8);
-		participacoesValues.put("ministracao_id", 0);
-		participacoesValues.put("participante_inscricao", 1003);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 9);
-		participacoesValues.put("ministracao_id", 1);
-		participacoesValues.put("participante_inscricao", 1003);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 10);
-		participacoesValues.put("ministracao_id", 2);
-		participacoesValues.put("participante_inscricao", 1003);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-		
-		participacoesValues.put("_id", 11);
-		participacoesValues.put("ministracao_id", 3);
-		participacoesValues.put("participante_inscricao", 1003);
-		participacoesValues.put("presenca", false);
-		participacoesValues.put("updated", false);
-		db.insert("participacao", null, participacoesValues);
-	}
+//	private void inserirDadosTeste(SQLiteDatabase db) {
+//		
+//		db.execSQL("DELETE FROM palestra;");
+//		db.execSQL("DELETE FROM ministracao;");
+//		db.execSQL("DELETE FROM participacao;");
+//		db.execSQL("DELETE FROM participante;");
+//		
+//		ContentValues values = new ContentValues();
+//		ContentValues ministracaoValues = new ContentValues();
+//		ContentValues participacoesValues = new ContentValues();
+//		
+//		//INSERINDO PALESTRAS E SUAS MINITRAÇÕES
+//		
+//		values.put("_id", 0);
+//		values.put("nome", "Desenvolvimento de Jogos");
+//		db.insert("palestra", null, values);
+//		
+//		ministracaoValues.put("_id", 0);
+//		ministracaoValues.put("data", "2014-11-03");
+//		ministracaoValues.put("palestra_id", 0);
+//		db.insert("ministracao", null, ministracaoValues);
+//		
+//		ministracaoValues.put("_id", 1);
+//		ministracaoValues.put("data", "2014-11-04");
+//		ministracaoValues.put("palestra_id", 0);
+//		db.insert("ministracao", null, ministracaoValues);
+//		
+//		values.put("_id", 1);
+//		values.put("nome", "Minicurso Arduino");
+//		db.insert("palestra", null, values);
+//		
+//		ministracaoValues.put("_id", 2);
+//		ministracaoValues.put("data", "2014-11-03");
+//		ministracaoValues.put("palestra_id", 1);
+//		db.insert("ministracao", null, ministracaoValues);
+//		
+//		ministracaoValues.put("_id", 3);
+//		ministracaoValues.put("data", "2014-11-04");
+//		ministracaoValues.put("palestra_id", 1);
+//		db.insert("ministracao", null, ministracaoValues);
+//		
+//		values.put("_id", 2);
+//		values.put("nome", "Introdução Ruby");
+//		db.insert("palestra", null, values);
+//		
+//		ministracaoValues.put("_id", 4);
+//		ministracaoValues.put("data", "2014-11-03");
+//		ministracaoValues.put("palestra_id", 2);
+//		db.insert("ministracao", null, ministracaoValues);
+//
+//		values.clear();
+//		
+//		//INSERINDO OS PARTICIPANTES E SUAS PARTICIPAÇÕES
+//		
+//		values.put("inscricao", 1001);
+//		values.put("nome", "João da Silva");
+//		db.insert("participante", null, values);
+//		
+//		participacoesValues.put("_id", 0);
+//		participacoesValues.put("ministracao_id", 0);
+//		participacoesValues.put("participante_inscricao", 1001);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 1);
+//		participacoesValues.put("ministracao_id", 1);
+//		participacoesValues.put("participante_inscricao", 1001);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 2);
+//		participacoesValues.put("ministracao_id", 2);
+//		participacoesValues.put("participante_inscricao", 1001);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 3);
+//		participacoesValues.put("ministracao_id", 3);
+//		participacoesValues.put("participante_inscricao", 1001);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 4);
+//		participacoesValues.put("ministracao_id", 4);
+//		participacoesValues.put("participante_inscricao", 1001);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		values.put("inscricao", 1002);
+//		values.put("nome", "Maria de Sousa Carvalho");
+//		db.insert("participante", null, values); //minist ids 2 e 3
+//		
+//		participacoesValues.put("_id", 5);
+//		participacoesValues.put("ministracao_id", 2);
+//		participacoesValues.put("participante_inscricao", 1002);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 6);
+//		participacoesValues.put("ministracao_id", 3);
+//		participacoesValues.put("participante_inscricao", 1002);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//	
+//		participacoesValues.put("_id", 7);
+//		participacoesValues.put("ministracao_id", 4);
+//		participacoesValues.put("participante_inscricao", 1002);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//	
+//		values.put("inscricao", 1003);
+//		values.put("nome", "Otavio Mesquita");
+//		db.insert("participante", null, values);
+//		//minist. ids 0, 1, 3 (jogos e ruby)
+//		
+//		participacoesValues.put("_id", 8);
+//		participacoesValues.put("ministracao_id", 0);
+//		participacoesValues.put("participante_inscricao", 1003);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 9);
+//		participacoesValues.put("ministracao_id", 1);
+//		participacoesValues.put("participante_inscricao", 1003);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 10);
+//		participacoesValues.put("ministracao_id", 2);
+//		participacoesValues.put("participante_inscricao", 1003);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//		
+//		participacoesValues.put("_id", 11);
+//		participacoesValues.put("ministracao_id", 3);
+//		participacoesValues.put("participante_inscricao", 1003);
+//		participacoesValues.put("presenca", false);
+//		participacoesValues.put("updated", false);
+//		db.insert("participacao", null, participacoesValues);
+//	}
 
 }
