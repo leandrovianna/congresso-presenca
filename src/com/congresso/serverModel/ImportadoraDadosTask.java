9a92366b76b2163c1cff2e0f2417d76169528a98
leandrovianna <leandrovianna50@gmail.com>
2014-11-17 15:50:34 -0200

    Colocando etapa 3 dentro do try...catch e usando finally para executar a etapa 4

--- src/com/congresso/serverModel/ImportadoraDadosTask.java
+++ src/com/congresso/serverModel/ImportadoraDadosTask.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.text.format.DateFormat;
@@ -47,10 +48,10 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 	@Override
 	protected void onPostExecute(Boolean result) {
 		super.onPostExecute(result);
-		
+
 		dao.close();
 		db.close();
-		
+
 		ac.retornoGravacaoDados(result);
 	}
 
@@ -61,6 +62,11 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 
 		json = json.replace("\\/", "-");
 		Evento evento = gson.fromJson(json, Evento.class);
+		
+		if (evento == null) {
+			Log.e("ImportadoraDadosTask", "ERRO: Leitura do Json - O objeto evento é null");
+			return false;
+		}
 
 		//1a fase: guardar as participações
 		List<Participacao> participacoesAntigas = dao.listarParticipacoesComPresenca();
@@ -72,132 +78,152 @@ public class ImportadoraDadosTask extends AsyncTask<String, String, Boolean>{
 		db.execSQL("DELETE FROM participante;");
 
 
-		//3a fase: incluir os dados do json
-		int ministracaoIdCount = 0; //contador para os ids das ministracoes
-		int participacaoIdCount = 0; //contador para os ids das participacoes
+		try {
+			//3a fase: incluir os dados do json
+			int ministracaoIdCount = 0; //contador para os ids das ministracoes
+			int participacaoIdCount = 0; //contador para os ids das participacoes
 
-		for (Atividade atividade : evento.getLISTA_ATIVIDADES()) {
+			for (Atividade atividade : evento.getLISTA_ATIVIDADES()) {
 
-			if (atividade == null)
-				break;
+				if (atividade == null)
+					break;
 
-			ContentValues values = new ContentValues(); //criando o contentvalues que será usado
-			String data;
+				ContentValues values = new ContentValues(); //criando o contentvalues que será usado
+				String data;
 
-			//fazendo processamento da data
-			try {
-				data = DateFormat.format("yyyy-MM-dd",
-						dateFormatJson.parse(atividade.getDTHORA_INICIO()))
-						.toString();
+				//fazendo processamento da data
+				try {
+					data = DateFormat.format("yyyy-MM-dd",
+							dateFormatJson.parse(atividade.getDTHORA_INICIO()))
+							.toString();
 
-			} catch (ParseException e) {
-				Log.e("ERRO: ImportadoraDados",
-						"Erro no parser da data a partir do Json");
-				return false;
-			}
-			Log.i("ImportadoraDadosTask", "Sucesso. Data a ser passado pro banco: "
-					+ data);
+				} catch (ParseException e) {
+					Log.e("ERRO: ImportadoraDados",
+							"Erro no parser da data a partir do Json");
+					return false;
+				}
+				Log.i("ImportadoraDadosTask", "Data a ser passado pro banco: "+data);
 
-
-			//verificar se a palestra NAO EXISTE
-			if ((db.rawQuery("SELECT * FROM palestra WHERE _id = "+atividade.getCODATIVIDADE(), 
-					null)).getCount() == 0) {
-
-				//se a palestra nao existe, então vamos adiciona-la ao banco
-
-				Log.i("ImportadoraDadosTask", "Criando Palestra cod:"+atividade.getCODATIVIDADE());
+				Cursor cursor = db.rawQuery("SELECT palestra._id FROM palestra WHERE _id = "+
+							atividade.getCODATIVIDADE(), null);
 				
-				//criando registro Palestra no banco
-				values.put("_id", Integer.parseInt(atividade.getCODATIVIDADE()));
-				values.put("nome", atividade.getATIVIDADE());
-				db.insert("palestra", null, values);
-				values.clear();
+				//verificar se a palestra NAO EXISTE
+				if (cursor.getCount() == 0) {
+					
+					//limpando cursor da memória, esses resultados não são mais necessário
+					cursor.close();
 
-				
-				//criando registro Ministracao no banco
-				values.put("_id", ministracaoIdCount);
-				values.put("data", data);
-				values.put("palestra_id",
-						Integer.parseInt(atividade.getCODATIVIDADE()));
-				db.insert("ministracao", null, values);
-				values.clear();
+					//se a palestra nao existe, então vamos adiciona-la ao banco
 
-				for (ParticipanteServer participante : atividade
-						.getLISTA_PARTICIPANTES()) {
+					Log.i("ImportadoraDadosTask", "Criando Palestra cod:"+atividade.getCODATIVIDADE());
 
-					if (participante == null)
-						break;
+					//criando registro Palestra no banco
+					values.put("_id", Integer.parseInt(atividade.getCODATIVIDADE()));
+					values.put("nome", atividade.getATIVIDADE());
+					db.insert("palestra", null, values);
+					values.clear();
 
-					if ((db.rawQuery("SELECT * FROM participante WHERE inscricao = "+participante.getCODPARTICIPANTE(), 
-							null)).getCount() == 0) {
 
-						Log.i("ImportadoraDadosTask", "Criando Participante cod:"+participante.getCODPARTICIPANTE());
+					//criando registro Ministracao no banco
+					values.put("_id", ministracaoIdCount);
+					values.put("data", data);
+					values.put("palestra_id",
+							Integer.parseInt(atividade.getCODATIVIDADE()));
+					db.insert("ministracao", null, values);
+					values.clear();
+
+					for (ParticipanteServer participante : atividade
+							.getLISTA_PARTICIPANTES()) {
+
+						if (participante == null)
+							break;
+
+						Cursor cursor2 = db.rawQuery("SELECT participante.inscricao FROM participante WHERE inscricao = "+
+											participante.getCODPARTICIPANTE(), null);
 						
-						//criando registro Participante no banco
-						values.put("inscricao",
+						if (cursor2.getCount() == 0) {
+							
+							//limpando cursor da memória, esses resultados não são mais necessário
+							cursor2.close();
+							
+							Log.i("ImportadoraDadosTask", "Criando Participante cod:"+participante.getCODPARTICIPANTE());
+
+							//criando registro Participante no banco
+							values.put("inscricao",
+									Integer.parseInt(participante.getCODPARTICIPANTE()));
+							values.put("nome", participante.getNOME());
+							db.insert("participante", null, values);
+							values.clear();
+
+						}
+
+						//criando registro Participacao no banco
+						values.put("_id", participacaoIdCount);
+						values.put("ministracao_id", ministracaoIdCount);
+						values.put("participante_inscricao",
 								Integer.parseInt(participante.getCODPARTICIPANTE()));
-						values.put("nome", participante.getNOME());
-						db.insert("participante", null, values);
+						values.put("presenca", false);
+						values.put("updated", false);
+						db.insert("participacao", null, values);
 						values.clear();
 
+						participacaoIdCount++;
 					}
+				} else {
+					//caso contrário, a palestra já existe no banco
+					//então vamos criar uma ministracao, com a nova data
+					
+					Log.i("ImportadoraDadosTask", "Palestra cod:"+atividade.getCODATIVIDADE()+" ja existe no banco!");
 
-					//criando registro Participacao no banco
-					values.put("_id", participacaoIdCount);
-					values.put("ministracao_id", ministracaoIdCount);
-					values.put("participante_inscricao",
-							Integer.parseInt(participante.getCODPARTICIPANTE()));
-					values.put("presenca", false);
-					values.put("updated", false);
-					db.insert("participacao", null, values);
+					//criando registro Ministracao no banco
+					values.put("_id", ministracaoIdCount);
+					values.put("data", data);
+					values.put("palestra_id",
+							Integer.parseInt(atividade.getCODATIVIDADE()));
+					db.insert("ministracao", null, values);
 					values.clear();
 
-					participacaoIdCount++;
+					for (ParticipanteServer participante : atividade
+							.getLISTA_PARTICIPANTES()) {
+
+						if (participante == null)
+							break;
+
+						//criando registro Participacao no banco
+						values.put("_id", participacaoIdCount);
+						values.put("ministracao_id", ministracaoIdCount);
+						values.put("participante_inscricao",
+								Integer.parseInt(participante.getCODPARTICIPANTE()));
+						values.put("presenca", false);
+						values.put("updated", false);
+						db.insert("participacao", null, values);
+						values.clear();
+
+						participacaoIdCount++;
+					}
 				}
-			} else {
-				//caso contrário, a palestra já existe no banco
-				//então vamos criar uma ministracao, com a nova data
 
-				//criando registro Ministracao no banco
-				values.put("_id", ministracaoIdCount);
-				values.put("data", data);
-				values.put("palestra_id",
-						Integer.parseInt(atividade.getCODATIVIDADE()));
-				db.insert("ministracao", null, values);
-				values.clear();
-
-				for (ParticipanteServer participante : atividade
-						.getLISTA_PARTICIPANTES()) {
-
-					if (participante == null)
-						break;
-
-					//criando registro Participacao no banco
-					values.put("_id", participacaoIdCount);
-					values.put("ministracao_id", ministracaoIdCount);
-					values.put("participante_inscricao",
-							Integer.parseInt(participante.getCODPARTICIPANTE()));
-					values.put("presenca", false);
-					values.put("updated", false);
-					db.insert("participacao", null, values);
-					values.clear();
-
-					participacaoIdCount++;
-				}
+				ministracaoIdCount++;
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 
-			ministracaoIdCount++;
-		}
-
-		//4a fase: reinserir participações antigas
-
-		for (Participacao participacao : participacoesAntigas) {
-			Log.i("ImportadoraDadosTask", "Backup: Atualizando Participacao");
-			Log.i("ImportadoraDadosTask", "ministracao_id = "+participacao.getMinistracao().getId()+
-					" participante nome:"+participacao.getParticipante().getNome()+
-					" inscricao:"+participacao.getParticipante().getInscricao());
+			Log.e("ImportadoraDadosTask", "ERRO: Ocorreu um erro na 3ª etapa - inserção dos dados no banco");
 			
-			dao.updateParticipacao(participacao);
+		} finally {
+
+			//4a fase: reinserir participações antigas
+
+			for (Participacao participacao : participacoesAntigas) {
+				Log.i("ImportadoraDadosTask", "Backup: Atualizando Participacao");
+				Log.i("ImportadoraDadosTask", "ministracao_id = "+participacao.getMinistracao().getId()+
+						" participante nome:"+participacao.getParticipante().getNome()+
+						" inscricao:"+participacao.getParticipante().getInscricao());
+
+				dao.updateParticipacao(participacao);
+			}
+			
+			Log.i("ImportadoraDadosTask", "Importação Terminada.");
 		}
 
 		return true;
